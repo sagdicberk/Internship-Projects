2dc544b2b85898642a02ee78a41c1cf493c47e12
Berk SAĞDIÇ <129013056+sagdicberk@users.noreply.github.com>
2024-07-26 12:30:41 +0300

    Update JwtUtil.java

--- task4/src/main/java/com/example/task4/core/util/jwt/JwtUtil.java
+++ task4/src/main/java/com/example/task4/core/util/jwt/JwtUtil.java
@@ -20,20 +20,19 @@ import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
-	// @Value("${jwt.key}")
+	
 	private final String secret = "a2V5IGZvciBqd3Qgc2lnbmF0dXJlIGFuZCB0b3BlIGJlIHNlY3VyZSBzdHJpbmc=";
 
-	// Generate token using UserDetails
+	
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
-        // Add roles to claims
+        
         claims.put("roles", userDetails.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList()));
         return createToken(claims, userDetails.getUsername());
     }
 
-    // Create token with claims and username
     private String createToken(Map<String, Object> claims, String username) {
         return Jwts.builder()
                 .setClaims(claims)
@@ -44,29 +43,25 @@ public class JwtUtil {
                 .compact();
     }
 
-    // Get signing key
+
     private Key getSignKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    // Extract username from token
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
 
-    // Extract expiration date from token
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    // Extract claims from token
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
-    // Extract all claims from token
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSignKey())
@@ -75,12 +70,10 @@ public class JwtUtil {
                 .getBody();
     }
 
-    // Check if token is expired
     private Boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
 
-    // Validate the token
     public boolean validateToken(String token, UserDetails userDetails) {
         final String username = extractUsername(token);
         if (isTokenExpired(token)) {
