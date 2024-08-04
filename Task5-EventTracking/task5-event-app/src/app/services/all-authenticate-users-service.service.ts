9512b5a7200770e08c19203c75d0bb99e3731d39
Berk SAĞDIÇ <129013056+sagdicberk@users.noreply.github.com>
2024-08-05 02:45:42 +0300

    Update all-authenticate-users-service.service.ts

--- Task5-EventTracking/task5-event-app/src/app/services/all-authenticate-users-service.service.ts
+++ Task5-EventTracking/task5-event-app/src/app/services/all-authenticate-users-service.service.ts
@@ -14,20 +14,20 @@ export class AllAuthenticateUsersServiceService {
   constructor(private http: HttpClient) { }
 
   private getAuthHeaders(): HttpHeaders {
-    const token = localStorage.getItem('authToken'); // JWT token'ı yerel depodan alın
+    const token = localStorage.getItem('authToken'); 
     return new HttpHeaders({
       'Authorization': `Bearer ${token}`
     });
   }
 
-  // Get notifications
+  
   getNotifications(): Observable<Notification[]> {
     return this.http.get<Notification[]>(`${this.baseUrl}/get/notifications`, {
       headers: this.getAuthHeaders()
     });
   }
 
-  // Join an event
+  
   joinEvent(eventId: number): Observable<string> {
     return this.http.post<string>(`${this.baseUrl}/${eventId}/join`, {}, {
       headers: this.getAuthHeaders(),
@@ -35,26 +35,26 @@ export class AllAuthenticateUsersServiceService {
     });
   }
 
-  // Respond to invitation
+  
   respondToInvitation(eventId: number, accept: boolean, notificationId: number): Observable<string> {
-    // Güncellenmiş URL ile POST isteği gönderme
+    
     return this.http.post<string>(`${this.baseUrl}/${eventId}/respond/${accept}/${notificationId}`, {}, {
       headers: this.getAuthHeaders(),
       responseType: 'text' as 'json'
     });
   }
 
-  // Get all categories
+  
   getAllCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(`${this.baseUrl}`);
   }
 
-  // Get upcoming events
+  
   getUpcomingEvents(): Observable<Event[]> {
     return this.http.get<Event[]>(`${this.baseUrl}/upcoming`);
   }
 
-  // Angular service method for getting events by category ID
+  
   getUpcomingEventsByCategoryId(categoryId: number): Observable<Event[]> {
     return this.http.get<Event[]>(`${this.baseUrl}/category/${categoryId}/upcoming`, {
       headers: this.getAuthHeaders()
@@ -63,7 +63,7 @@ export class AllAuthenticateUsersServiceService {
 
 
 
-  // Get events in next week
+  
   getEventsInNextWeek(): Observable<Event[]> {
     return this.http.get<Event[]>(`${this.baseUrl}/next-week`);
   }
